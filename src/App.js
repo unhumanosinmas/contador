ff9a7ebe898823bd8d6c9f91b5a43f62f394bab7
Diego Enrique Hernández Cholotío <104159636+unhumanosinmas@users.noreply.github.com>
2022-04-23 13:05:14 -0600

    contador base
    Se creo un contador base, sin mayor diseño

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [contador, setContador] = useState(6);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <div className="card text-center my-5">
+      <div className="card-body">
+      <h1>Aplicacion Contador</h1>
+      <div className="my-5">
+        <h2 className="my-5">{contador}</h2>
+        <button className="btn btn-success mx-3" onClick={() => setContador(contador + 1)}>Aumentar</button>
+        <button className="btn btn-secondary mx-3" onClick={() => setContador(0)}>Reiniciar</button>
+        <button className="btn btn-danger mx-3" onClick={() => setContador(contador - 1)}>Disminuir</button>
+      </div>
+      </div>
+      </div>
     </div>
   );
 }
