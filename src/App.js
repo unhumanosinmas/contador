4511b7b8908ecc7189c60b0f354d4d63200c54d9
Diego Enrique Hernández Cholotío <104159636+unhumanosinmas@users.noreply.github.com>
2022-04-23 13:17:27 -0600

    Color contador
    Se le dió color al número del contador, verde para positivo,  gris para 0, y rojo para negativo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [contador, setContador] = useState(6);
+  const [contador, setContador] = useState(0);
+  
+  function blanco() {
+    if (contador === 0) {
+      document.getElementById('contadorId').style.color = "gray";
+    } if (contador > 0) {
+      document.getElementById('contadorId').style.color = "green";
+    } 
+    if (contador < 0) {
+      document.getElementById('contadorId').style.color = "red";
+    } 
+  }
+
+  React.useEffect(()=>{
+    blanco();
+  })
+  
   return (
     <div className="container my-5">
       <div className="card text-center my-5">
-      <div className="card-body">
-      <h1>Aplicacion Contador</h1>
-      <div className="my-5">
-        <h2 className="my-5">{contador}</h2>
-        <button className="btn btn-success mx-3" onClick={() => setContador(contador + 1)}>Aumentar</button>
-        <button className="btn btn-secondary mx-3" onClick={() => setContador(0)}>Reiniciar</button>
-        <button className="btn btn-danger mx-3" onClick={() => setContador(contador - 1)}>Disminuir</button>
-      </div>
-      </div>
+        <div className="card-body">
+          <h1>Aplicacion Contador</h1>
+          <div className="my-5">
+            <h2 id="contadorId" className="my-5">{contador}</h2>
+            <button
+              className="btn btn-success mx-3"
+              onClick={() => setContador(contador + 1)}
+            >
+              Aumentar
+            </button>
+            <button
+              className="btn btn-secondary mx-3"
+              onClick={() => setContador(0)}
+              disabled={contador===0}
+            >
+              Reiniciar
+            </button>
+            <button
+              className="btn btn-danger mx-3"
+              onClick={() => setContador(contador - 1)}
+            >
+              Disminuir
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
